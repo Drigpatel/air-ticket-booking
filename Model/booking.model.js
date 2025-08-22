b3fd99a5b1413313f8e0a11eecd4bb0ec7f87074
Drigpatel <pdirg066@gmail.com>
2025-08-22 16:23:09 -0400

    Update booking.model

--- Model/booking.model.js
+++ Model/booking.model.js
@@ -1,7 +1,1 @@
-const mongoose = require('mongoose');
-const bookingSchema = mongoose.Schema({
-    user: { type: String, ref: 'User' },
-    flight: { type: String, ref: 'Flight' }
-})
-const BookingModel = mongoose.model('booking', bookingSchema)
-module.exports = BookingModel;
+
